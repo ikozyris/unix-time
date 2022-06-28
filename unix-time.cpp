26f3f200cfb591ed649d7f1f0336bff68f2be56c
ikozyris <80053394+ikozyris@users.noreply.github.com>
2022-06-28 17:33:06 +0300

    Update unix-time.cpp

--- unix-time.cpp
+++ unix-time.cpp
@@ -29,17 +29,15 @@ using namespace std;
 
 int main()
 {
-   cout << "\033[2J\033[1;1H"; //clear screen escape code
-    printf("Created by ikozyris.  @ikozyris on Github and Gitlab. This is a simple program that prints the current timestamp since the Unix Epoch. Also, it will start now!\n");
-    sleep(7);
+   cout << "\033[2J\033[1;1H"; //clear screen escape code + move cursor to 1,1
 
     while (1)
     {
     time_t result = time(nullptr); //time_t sets result to unix time
-    cout << asctime(localtime(&result))
-              << result << " seconds since the Unix Epoch\n";
+    cout << asctime(localtime(&result)) // sets localtime (defined by time_t) to result
+         << result << " seconds since the Unix Epoch\n";
     sleep(1);
-    cout << "\033[2J\033[1;1H";
+    cout << "\033[2J\033[1;1H"; //clear screen escape code + move cursor to 1,1
     }
 
 }
